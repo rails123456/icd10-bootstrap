07d628c840e69c846ac23b0284eee18b6fcc5560
admin <admin@localhost.localdomain>
2012-03-20 21:27:25 +0700

    recheck migrate faqs

--- db/migrate/20120319110139_create_faqs.rb
+++ db/migrate/20120319110139_create_faqs.rb
@@ -2,8 +2,8 @@ class CreateFaqs < ActiveRecord::Migration
   def change
     create_table :faqs do |t|
       t.string :faq_item
-      t.string :faq_q
-      t.string :faq_a
+      t.text :faq_q
+      t.text :faq_a
     end
   end
 end
